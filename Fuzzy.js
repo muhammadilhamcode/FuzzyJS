0bdb6af0ecc11c72066dd91943de6416ae570cf4
muhammadilhamcode <muhammadilhamabdulgani@gmail.com>
2025-12-25 01:14:02 +0800

    Update Fuzzy.js

--- Fuzzy.js
+++ Fuzzy.js
@@ -2,9 +2,11 @@ var fuzzyjs = {
    min_value : "null",
    max_value : "null",
    fuzzy_linguistic_var : false,
+   // fuzzy linguistic variable terdeteksi
    fuzzy_linguistic_detected : function(){
      this.fuzzy_linguistic_var = true;
    },
+   // menetralkan fuzzy linguistic variable
    fuzzy_linguistic_neuturalize : function(){
      this.fuzzy_linguistic_var = false;
    }
