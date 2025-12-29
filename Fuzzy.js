5c87aadbaa0dff7c4c22103e13434afdea544e23
muhammadilhamcode <muhammadilhamabdulgani@gmail.com>
2025-12-29 10:07:04 +0800

    Update Fuzzy.js

--- Fuzzy.js
+++ Fuzzy.js
@@ -31,7 +31,7 @@ var fuzzyjs = {
    },
    // set keadaan fuzzy
    set_fuzzy : function(fuzzy_val){
-      // apabila fuzzy ling variable terdeteksi 
+       // apabila fuzzy ling variable terdeteksi 
       /* set atau add (apabila di upgrade ke array) status fuzzy */
       // aktivkan fuzzy linguistic variable
       this.fuzzy_linguistic_activate();
